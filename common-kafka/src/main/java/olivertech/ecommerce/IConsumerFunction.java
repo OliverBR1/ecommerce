f8af95b3ca9baf23fb44c21e67ca71b93588f06c
OliverBR1 <Br.0liveira@outlook.com>
2025-05-18 16:10:46 -0300

    first commit

---
+++ common-kafka/src/main/java/olivertech/ecommerce/IConsumerFunction.java
@@ -0,0 +1,7 @@
+package olivertech.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface IConsumerFunction<T> {
+    void consume(ConsumerRecord<String, T> record);
+}

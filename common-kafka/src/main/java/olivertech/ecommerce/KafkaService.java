4abb615310e12647827794e6229d0b509ac9e291
OliverBR1 <Br.0liveira@outlook.com>
2025-05-24 23:49:45 -0300

    Update project

--- common-kafka/src/main/java/olivertech/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/olivertech/ecommerce/KafkaService.java
@@ -14,19 +14,19 @@ import java.util.regex.Pattern;
 
 public class KafkaService<T> implements Closeable {
     private final KafkaConsumer<String, T> consumer;
-    private final ConsumerFunction parse;
+    private final IConsumerFunction parse;
 
-    KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type, Map<String,String> properties) {
+    KafkaService(String groupId, String topic, IConsumerFunction parse, Class<T> type, Map<String,String> properties) {
         this(parse, groupId, type, properties);
         consumer.subscribe(Collections.singletonList(topic));
     }
 
-    KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type, Map<String,String> properties) {
+    KafkaService(String groupId, Pattern topic, IConsumerFunction parse, Class<T> type, Map<String,String> properties) {
         this(parse, groupId, type, properties);
         consumer.subscribe(topic);
     }
 
-    private KafkaService(ConsumerFunction parse, String groupId, Class<T> type, Map<String, String> properties) {
+    private KafkaService(IConsumerFunction parse, String groupId, Class<T> type, Map<String, String> properties) {
         this.parse = parse;
         this.consumer = new KafkaConsumer<>(getProperties(type, groupId, properties));
     }

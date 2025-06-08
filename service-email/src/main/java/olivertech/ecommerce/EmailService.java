9b594d3caaa4f558f304e4f4933b048e8e881863
OliverBR1 <Br.0liveira@outlook.com>
2025-06-08 02:12:13 -0300

    updated classes and new file

--- service-email/src/main/java/olivertech/ecommerce/EmailService.java
+++ service-email/src/main/java/olivertech/ecommerce/EmailService.java
@@ -7,18 +7,17 @@ import java.util.concurrent.ExecutionException;
 
 public class EmailService {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException{
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         var emailService = new EmailService();
         try (var service = new KafkaService(EmailService.class.getSimpleName(),
                 "ECOMMERCE_SEND_EMAIL",
                 emailService::parse,
-                String.class,
                 Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Message<String>> record) {
         System.out.println("------------------------------------------");
         System.out.println("Send email");
         System.out.println(record.key());
